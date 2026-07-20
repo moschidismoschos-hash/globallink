00d54c8f449165b7296f71ea140a4f303851c0c4
moschidismoschos-hash <moschidismoschos@gmail.com>
2026-07-20 21:42:03 +0300

    Διόρθωση περιστροφής Γης και εμφάνιση ID CD28424

---
+++ professional-preview/assets/config.js
@@ -0,0 +1,5 @@
+window.GLOBALLINK_CONFIG = {
+  // Πρόσθεσε αργότερα το GA4 Measurement ID, π.χ. "G-XXXXXXXXXX".
+  // Μέχρι τότε, δεν φορτώνεται εξωτερικό σύστημα αναλυτικών.
+  gaMeasurementId: ""
+};
