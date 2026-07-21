6e74cf64219ccf3ed04a54efee08fc0cf07f3265
moschidismoschos-hash <moschidismoschos@gmail.com>
2026-07-21 12:41:15 +0300

    GlobalLink μία οθόνη χωρίς σκρολ

--- professional-preview/assets/config.js
+++
@@ -1,5 +0,0 @@
-window.GLOBALLINK_CONFIG = {
-  // Πρόσθεσε αργότερα το GA4 Measurement ID, π.χ. "G-XXXXXXXXXX".
-  // Μέχρι τότε, δεν φορτώνεται εξωτερικό σύστημα αναλυτικών.
-  gaMeasurementId: ""
-};
